5422948c19f2d3170255abae013131f05e50e710
Dae Kim <daehk4@uci.edu>
2020-02-05 20:57:18 -0800

    Update grades using DOM Manipulation - Dae Kim

--- components/grade-table.js
+++ components/grade-table.js
@@ -3,6 +3,20 @@ class GradeTable {
     this.tableElement = tableElement;
   }
   updateGrades(grades) {
-    console.log(grades);
+    var tableBody = document.querySelector("tbody");
+    tableBody.textContent = "";
+    for (var i = 0; i < grades.length; i++) {
+      var tableRow = document.createElement("tr");
+      var studentName = document.createElement("td");
+      var studentCourse = document.createElement("td");
+      var studentGrade = document.createElement("td");
+      studentName.textContent = grades[i].name;
+      studentCourse.textContent = grades[i].course;
+      studentGrade.textContent = grades[i].grade;
+      tableRow.appendChild(studentName);
+      tableRow.appendChild(studentCourse);
+      tableRow.appendChild(studentGrade);
+      tableBody.appendChild(tableRow);
+    }
   }
 }
