8be489d20fcdaad51364183b09f6e746f4967636
Dae Kim <daehk4@uci.edu>
2020-02-05 18:13:24 -0800

    Requesting Data complete - Dae Kim

--- components/app.js
+++ components/app.js
@@ -1,3 +1,25 @@
 class App {
-
+  constructor() {
+    this.handleGetGradesError.bind(this);
+    this.handleGetGradesSuccess.bind(this);
+  }
+  handleGetGradesError(error) {
+    console.error(error);
+  }
+  handleGetGradesSuccess(grades) {
+    console.log(grades);
+  }
+  getGrades() {
+    $.ajax({
+      method: "GET",
+      url: "http://sgt.lfzprototypes.com/api/grades",
+      data: "none",
+      headers: {"X-Access-Token":"HkFiZOPJ"},
+      success: this.handleGetGradesSuccess,
+      error: this.handleGetGradesError,
+    })
+  }
+  start() {
+    this.getGrades();
+  }
 }
