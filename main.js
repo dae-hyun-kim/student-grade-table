8a46a1487fbd3ca640cdbd54fea2d98aa349982c
Dae Kim <daehk4@uci.edu>
2020-02-06 10:47:59 -0800

    Selected and stored header element / instantiated pageHeader - Dae Kim

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+var header = document.querySelector("header");
+var pageHeader = new PageHeader(header);
 var tableElement = document.querySelector("table");
 var gradeTable = new GradeTable(tableElement);
 var app = new App(gradeTable);
