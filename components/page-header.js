f3fd9e18fb883a4af7456b0527cbf991d1e76121
Dae Kim <daehk4@uci.edu>
2020-02-06 11:02:05 -0800

    Dynamically update grade average badge complete - Dae Kim

--- components/page-header.js
+++ components/page-header.js
@@ -3,6 +3,7 @@ class PageHeader {
     this.headerElement = headerElement;
   }
   updateAverage(newAverage) {
-    console.log(newAverage);
+    var averageBadge = this.headerElement.querySelector(".badge");
+    averageBadge.textContent = newAverage;
   }
 }
