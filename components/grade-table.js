f771ca2590f5a978082b399542dc193a3066165c
Dae Kim <daehk4@uci.edu>
2020-02-06 17:57:53 -0800

    modified GradeTables update method with rendergraderow - Dae Kim

--- components/grade-table.js
+++ components/grade-table.js
@@ -7,23 +7,14 @@ class GradeTable {
     var tableBody = this.tableElement.querySelector("tbody");
     tableBody.textContent = "";
     for (var i = 0; i < grades.length; i++) {
-      var tableRow = document.createElement("tr");
-      var studentName = document.createElement("td");
-      var studentCourse = document.createElement("td");
-      var studentGrade = document.createElement("td");
-      studentName.textContent = grades[i].name;
-      studentCourse.textContent = grades[i].course;
-      studentGrade.textContent = grades[i].grade;
-      tableRow.appendChild(studentName);
-      tableRow.appendChild(studentCourse);
-      tableRow.appendChild(studentGrade);
-      tableBody.appendChild(tableRow);
+      this.renderGradeRow(grades[i], this.deleteGrade);
     }
   }
   onDeleteClick(deleteGrade) {
     this.deleteGrade = deleteGrade;
   }
   renderGradeRow(data, deleteGrade) {
+    var tableBody = this.tableElement.querySelector("tbody");
     var tableRow = document.createElement("tr");
     var tableDataName = document.createElement("td");
     var tableDataCourse = document.createElement("td");
@@ -32,7 +23,9 @@ class GradeTable {
     var tableDeleteButton = document.createElement("button");
     tableDeleteButton.classList.add("btn", "btn-danger");
     tableDeleteButton.textContent = "DELETE"
-    tableDeleteButton.addEventListener("click", deleteGrade(data.id))
+    tableDeleteButton.addEventListener("click", function(){
+      deleteGrade(data.id);
+    })
     tableDataDelete.appendChild(tableDeleteButton);
 
     tableDataName.textContent = data.name;
@@ -43,6 +36,6 @@ class GradeTable {
     tableRow.appendChild(tableDataCourse);
     tableRow.appendChild(tableDataGrade);
 
-    return tableRow;
+    tableBody.appendChild(tableRow);
   }
 }
