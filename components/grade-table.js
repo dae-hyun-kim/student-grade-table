5ce4198cb3a10e87c4ec0af085ee1d71e94c88a8
Dae Kim <daehk4@uci.edu>
2020-02-06 17:20:37 -0800

    added onDeleteClick method onto GradeTable class - Dae Kim

--- components/grade-table.js
+++ components/grade-table.js
@@ -1,6 +1,7 @@
 class GradeTable {
   constructor(tableElement) {
     this.tableElement = tableElement;
+    this.deleteGrade = null;
   }
   updateGrades(grades) {
     var tableBody = this.tableElement.querySelector("tbody");
@@ -18,5 +19,8 @@ class GradeTable {
       tableRow.appendChild(studentGrade);
       tableBody.appendChild(tableRow);
     }
+  }
+  onDeleteClick(deleteGrade) {
+    this.deleteGrade = deleteGrade;
   }
 }
