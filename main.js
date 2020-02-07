4e3adeae7ebc2787bb51b5b5d4190bf30332b23b
Dae Kim <daehk4@uci.edu>
2020-02-07 09:10:28 -0800

    query for noGradeRecorded element and passed it as a parameter to GradeTable class - Dae Kim

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 var header = document.querySelector("header");
 var pageHeader = new PageHeader(header);
+var noGradeRecorded = document.getElementById("no-grades");
 var tableElement = document.querySelector("table");
-var gradeTable = new GradeTable(tableElement);
+var gradeTable = new GradeTable(tableElement, noGradeRecorded);
 var form = document.querySelector("form");
 var gradeForm = new GradeForm(form);
 var app = new App(gradeTable, pageHeader, gradeForm);
